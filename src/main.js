09b6244246d7ecc1389207afa9a363ee7556fc39
Huijiny <jiin20803@gmail.com>
2021-08-02 16:24:47 +0900

    🎉 Init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueGlide from 'vue-glide-js'
+import 'vue-glide-js/dist/vue-glide.css'
 
+Vue.use(VueGlide)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
