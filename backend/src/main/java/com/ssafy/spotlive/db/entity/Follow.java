94935d2b9c2c42be0850dd3d10ad620c612c6af4
ahhyeon <ahyendkgus@gmail.com>
2021-08-02 19:40:49 +0900

    🔧 Add Follow Entity

---
+++ backend/src/main/java/com/ssafy/spotlive/db/entity/Follow.java
@@ -0,0 +1,23 @@
+package com.ssafy.spotlive.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@IdClass(FollowId.class)
+public class Follow{
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fan")
+    User fan;
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artist")
+    User artist;
+}
