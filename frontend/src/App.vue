71449fa876d76e09f1e6b4de397f0be272ecc79f
Huijiny <jiin20803@gmail.com>
2021-08-02 20:05:45 +0900

    🎨 Add all icons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/main">Main</router-link> 
     </div>
     <router-view/>
   </div>
@@ -10,13 +9,13 @@
 
 <style>
 body {
+  /* 메인 배경 */
   background-color: #121212;
 }
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale; 
   text-align: center;
   color: #2c3e50;
 }
