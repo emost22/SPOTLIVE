70ee8f3a1e42559f2d19196157679b45443273b1
ysys <emoney96@naver.com>
2021-08-02 22:36:22 +0900

    🔧 Add CategoryGetResponseDto of Method

--- backend/src/main/java/com/ssafy/spotlive/api/response/CategoryGetRes.java
+++ backend/src/main/java/com/ssafy/spotlive/api/response/CategoryGetRes.java
@@ -1,5 +1,6 @@
 package com.ssafy.spotlive.api.response;
 
+import com.ssafy.spotlive.db.entity.Category;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,4 +18,16 @@ import lombok.ToString;
 public class CategoryGetRes {
     Long categoryId;
     String categoryName;
+
+    public static CategoryGetRes of(Category category){
+        /**
+         * @Method Name : of
+         * @작성자 : 강용수
+         * @Method 설명 : Category Entity를 CategoryGetResponseDto로 변환하는 메소드
+         */
+        return CategoryGetRes.builder()
+                .categoryId(category.getCategoryId())
+                .categoryName(category.getCategoryName())
+                .build();
+    }
 }
