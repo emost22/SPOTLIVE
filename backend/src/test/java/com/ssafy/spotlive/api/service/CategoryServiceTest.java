c4b014c2df9d88a810d0ca92223fd1f905994866
ysys <emoney96@naver.com>
2021-08-03 15:33:51 +0900

    🔧 Add CategoryServiceTest findAllCategory Method

--- backend/src/test/java/com/ssafy/spotlive/api/service/CategoryServiceTest.java
+++ backend/src/test/java/com/ssafy/spotlive/api/service/CategoryServiceTest.java
@@ -1,10 +1,23 @@
 package com.ssafy.spotlive.api.service;
 
+import com.ssafy.spotlive.api.response.category.CategoryGetRes;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CategoryServiceTest {
     @Autowired
     CategoryService categoryService;
+
+    @Test
+    void findAllCategory() {
+        List<CategoryGetRes> categoryGetResList = categoryService.findAllCategory();
+
+        assertEquals(categoryGetResList.size(), 6);
+    }
 }
