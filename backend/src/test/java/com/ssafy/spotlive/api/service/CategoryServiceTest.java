bbe851d58cb86aa042c5a0c2a907faf90273dc13
ysys <emoney96@naver.com>
2021-08-03 15:33:20 +0900

    🔧 Create CategoryServiceTest

---
+++ backend/src/test/java/com/ssafy/spotlive/api/service/CategoryServiceTest.java
@@ -0,0 +1,10 @@
+package com.ssafy.spotlive.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceTest {
+    @Autowired
+    CategoryService categoryService;
+}
