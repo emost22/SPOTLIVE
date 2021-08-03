ca825718a036d4a225d44c9c5f3e0ed9e20918ed
ysys <emoney96@naver.com>
2021-08-03 14:42:11 +0900

    🔨 Fix CategoryRepository findCategoryByCategoryName Method Change return type Category -> Optional<Category>

--- backend/src/main/java/com/ssafy/spotlive/db/repository/CategoryRepository.java
+++ backend/src/main/java/com/ssafy/spotlive/db/repository/CategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @FileName : CategoryRepository
@@ -25,5 +26,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
      * @작성자 : 강용수
      * @Method 설명 : 카테고리를 카테고리 이름으로 조회하는 메소드
      */
-    Category findCategoryByCategoryName(String categoryName);
+    Optional<Category> findCategoryByCategoryName(String categoryName);
 }
