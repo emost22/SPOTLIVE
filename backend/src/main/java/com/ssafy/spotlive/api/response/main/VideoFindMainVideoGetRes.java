54f93ae9d14309ff475d022f0d56958a7f82ed1f
ysys <emoney96@naver.com>
2021-08-03 22:35:24 +0900

    🔧 Add VideoFindMainVideoGetResponseDto of Method

--- backend/src/main/java/com/ssafy/spotlive/api/response/main/VideoFindMainVideoGetRes.java
+++ backend/src/main/java/com/ssafy/spotlive/api/response/main/VideoFindMainVideoGetRes.java
@@ -1,5 +1,6 @@
 package com.ssafy.spotlive.api.response.main;
 
+import com.ssafy.spotlive.db.entity.Video;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,4 +31,26 @@ public class VideoFindMainVideoGetRes {
     VideoFindUserRes user;
     LocalDateTime startTime;
     String categoryName;
+
+    public static VideoFindMainVideoGetRes of(Video video, String videoLength){
+        /**
+         * @Method Name : of
+         * @작성자 : 강용수
+         * @Method 설명 : Video Entity와 videoLength를 VideoFindMainVideoGetResponseDto로 변환하는 메소드
+         */
+        return VideoFindMainVideoGetRes.builder()
+                .videoId(video.getVideoId())
+                .videoTitle(video.getVideoTitle())
+                .videoDescription(video.getVideoDescription())
+                .mode(video.getMode())
+                .thumbnailUrl(video.getThumbnailUrl())
+                .videoUrl(video.getVideoUrl())
+                .videoLength(videoLength)
+                .isLive(video.getIsLive())
+                .hit(video.getHit())
+                .user(VideoFindUserRes.of(video.getUser()))
+                .startTime(video.getStartTime())
+                .categoryName(video.getCategory().getCategoryName())
+                .build();
+    }
 }
