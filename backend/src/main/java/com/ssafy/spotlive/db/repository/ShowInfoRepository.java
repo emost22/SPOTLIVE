25d713e39228903767da9587d5a824a65cb85da2
ahhyeon <ahyendkgus@gmail.com>
2021-08-03 16:48:17 +0900

    💡  Add comments in ShowInfoRepository

--- backend/src/main/java/com/ssafy/spotlive/db/repository/ShowInfoRepository.java
+++ backend/src/main/java/com/ssafy/spotlive/db/repository/ShowInfoRepository.java
@@ -4,6 +4,11 @@ import com.ssafy.spotlive.db.entity.ShowInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @FileName : ShowInfoRepository
+ * @작성자 : 금아현
+ * @Class 설명 : ShowInfo 모델 관련 디비 쿼리 생성을 위한 JPA Query Method 인터페이스 정의.
+ */
 @Repository
 public interface ShowInfoRepository extends JpaRepository<ShowInfo, Long> {
 
