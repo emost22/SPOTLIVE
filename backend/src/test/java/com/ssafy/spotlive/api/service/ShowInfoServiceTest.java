a6c34634a984fe2193120b40b8d79d489f2f34c5
ahhyeon <ahyendkgus@gmail.com>
2021-08-03 19:58:53 +0900

    🔧 Create ShowInfoServiceTest and Add findShowInfoById(Service Layer) test code

---
+++ backend/src/test/java/com/ssafy/spotlive/api/service/ShowInfoServiceTest.java
@@ -0,0 +1,37 @@
+package com.ssafy.spotlive.api.service;
+
+import com.ssafy.spotlive.db.repository.ShowInfoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ShowInfoServiceTest {
+
+    @Mock
+    ShowInfoRepository showInfoRepository;
+
+    @InjectMocks
+    ShowInfoService showInfoService = new ShowInfoServiceImpl();
+
+    @Test
+    void insertShowInfo() {
+
+    }
+
+    @Test
+    void findShowInfoById() {
+//        ShowInfo showInfo = new ShowInfo();
+//        showInfo.setShowInfoId(1L);
+//        given(showInfoRepository.findShowInfoByShowInfoId(-1L)).willReturn(Optional.of(showInfo));
+//        ShowInfoFindByIdGetRes expected = showInfoService.findShowInfoById(-1L);
+//        assertThat(expected).isNotNull();
+
+        long showInfoId = 1L;
+        showInfoService.findShowInfoById(showInfoId);
+        verify(showInfoRepository).findShowInfoByShowInfoId(showInfoId);
+    }
+}
