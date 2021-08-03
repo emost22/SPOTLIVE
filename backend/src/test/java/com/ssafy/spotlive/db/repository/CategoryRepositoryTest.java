b5e43105ea6a7858477da1f8c331f0812bbe51b1
ysys <emoney96@naver.com>
2021-08-03 15:32:30 +0900

    🔧 Add CategoryRepositoryTest findAll Method

--- backend/src/test/java/com/ssafy/spotlive/db/repository/CategoryRepositoryTest.java
+++ backend/src/test/java/com/ssafy/spotlive/db/repository/CategoryRepositoryTest.java
@@ -1,10 +1,23 @@
 package com.ssafy.spotlive.db.repository;
 
+import com.ssafy.spotlive.db.entity.Category;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CategoryRepositoryTest {
     @Autowired
     CategoryRepository categoryRepository;
+
+    @Test
+    void findAll() {
+        List<Category> categoryList = categoryRepository.findAll();
+
+        assertEquals(categoryList.size(), 6);
+    }
 }
