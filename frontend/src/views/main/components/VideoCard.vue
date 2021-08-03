46b64f1b7f0840ec065be256f36ac506d4202969
MinahLee <ma0723@naver.com>
2021-08-03 14:38:02 +0900

    🔧Add card at VideoCard.vue

--- frontend/src/views/main/components/VideoCard.vue
+++ frontend/src/views/main/components/VideoCard.vue
@@ -1,25 +1,38 @@
 <template>
-  <div class="txtcolor-white">
+  <div class="container px-0">
+    <div class="col">
+      <div class="card" @click="RoomDetail" style="height:200px; width:400px;">
+        <img :src="video.thumbnailUrl" style="height:300px;">
+        <div class="card-body" style="overflow:hidden;">
+          <p class="card-title fw-bold text-start text-nowrap overflow-hidden">{{ video.title }} 제목</p>
+          <p class="card-title text-start">
+            {{ video.user_id }} 송출자
+          </p> 
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Main",
-  components: {
-  },
-  data: function () {
-    return {
-    }
-  },
-  methods: {
-    getRecommend: function () {
+  name: "VideoCard",
+  props: {
+    video: {
+      type: Object,
+      required: true
     },
   },
-  created: function () {
-    this.getRecommend()
+  methods: {
+    RoomDetail: function () {
+      if (localStorage.getItem('jwt')) {
+        this.$router.push({ name: 'RoomDetail', query: { video: this.video } })
+      } else {
+        this.$router.push({name: 'Login'})
+      }
+    },
   },
-  computer: function () {
+  computed: {
   },
 }
 </script>
