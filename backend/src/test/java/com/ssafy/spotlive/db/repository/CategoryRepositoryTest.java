f030a8a284c9e3d0074fd4332a02d84a1930978b
ysys <emoney96@naver.com>
2021-08-03 15:31:58 +0900

    🔧 Create CategoryRepositoryTest

---
+++ backend/src/test/java/com/ssafy/spotlive/db/repository/CategoryRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.ssafy.spotlive.db.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+    @Autowired
+    CategoryRepository categoryRepository;
+}
