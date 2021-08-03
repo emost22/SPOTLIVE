44c2c0e0edaf8b75767015f5c0256377adc96799
kimminkwon <kmk130519@gmail.com>
2021-08-03 14:22:41 +0900

    🔨 Modify Spring Security Config for testing

--- backend/src/main/java/com/ssafy/spotlive/config/WebSecurityConfig.java
+++ backend/src/main/java/com/ssafy/spotlive/config/WebSecurityConfig.java
@@ -10,8 +10,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/oauth2/**").permitAll()
-                .anyRequest().authenticated()
-                .and().oauth2Login();
+                .antMatchers("/api/auth/kakao/**").permitAll()
+                .anyRequest().authenticated();
     }
 }
