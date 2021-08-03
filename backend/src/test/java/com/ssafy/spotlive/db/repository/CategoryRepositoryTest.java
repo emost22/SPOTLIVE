e04e3dbc0b730b3a81222789743b1e47fc83d4a0
ysys <emoney96@naver.com>
2021-08-03 19:32:43 +0900

    🔥 Remove CategoryRepositoryTest findAll Method

--- backend/src/test/java/com/ssafy/spotlive/db/repository/CategoryRepositoryTest.java
+++ backend/src/test/java/com/ssafy/spotlive/db/repository/CategoryRepositoryTest.java
@@ -5,22 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CategoryRepositoryTest {
     @Autowired
     CategoryRepository categoryRepository;
-
-    @Test
-    void findAll() {
-        List<Category> categoryList = categoryRepository.findAll();
-
-        assertEquals(categoryList.size(), 6);
-    }
 
     @Test
     void findCategoryByCategoryName(){
