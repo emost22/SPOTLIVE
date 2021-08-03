1d6019b8d0acaed5869b847c9800e9a8a0bb5deb
ysys <emoney96@naver.com>
2021-08-03 19:45:06 +0900

    🔧 Add CategoryServiceImplTest findAllCategory Method

--- backend/src/test/java/com/ssafy/spotlive/api/service/CategoryServiceImplTest.java
+++ backend/src/test/java/com/ssafy/spotlive/api/service/CategoryServiceImplTest.java
@@ -1,10 +1,14 @@
 package com.ssafy.spotlive.api.service;
 
 import com.ssafy.spotlive.db.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceImplTest {
@@ -13,4 +17,15 @@ class CategoryServiceImplTest {
 
     @InjectMocks
     private CategoryServiceImpl categoryServiceImpl;
+
+    @Test
+    void findAllCategory(){
+        // given x
+
+        // when
+        categoryServiceImpl.findAllCategory();
+
+        // then
+        verify(categoryRepository).findAll();
+    }
 }
