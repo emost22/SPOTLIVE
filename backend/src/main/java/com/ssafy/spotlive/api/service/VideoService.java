803177a8a16a3311ba3d7424399aa6321ae07d3a
sqk8657 <sqk86577@gmail.com>
2021-08-03 17:40:50 +0900

    🔧 Add VideoService for insertVideo

---
+++ backend/src/main/java/com/ssafy/spotlive/api/service/VideoService.java
@@ -0,0 +1,14 @@
+package com.ssafy.spotlive.api.service;
+
+import com.ssafy.spotlive.api.request.video.VideoInsertPostReq;
+import com.ssafy.spotlive.api.response.video.VideoInsertPostRes;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @FileName : VideoService
+ * @작성자 : 권영린
+ * @Class 설명 : Video관련 기능을 위한 Service 정의.
+ */
+public interface VideoService {
+    VideoInsertPostRes insertVideo(VideoInsertPostReq videoInsertPostReq, MultipartFile thumbnailImage);
+}
