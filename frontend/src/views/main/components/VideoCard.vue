74260a6aa9c606e8682b21cda374b20df9562bed
MinahLee <ma0723@naver.com>
2021-08-03 14:07:56 +0900

    🔧Add VideoCard.vue components of main folder

---
+++ frontend/src/views/main/components/VideoCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="txtcolor-white">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  components: {
+  },
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+    getRecommend: function () {
+    },
+  },
+  created: function () {
+    this.getRecommend()
+  },
+  computer: function () {
+  },
+}
+</script>
+
+<style>
+
+</style>
