a3a378582d0f46aef2baf92d6786ed8d38ad13bf
ysys <emoney96@naver.com>
2021-08-04 18:51:09 +0900

    🔧 Add MainServiceImpl findAllLiveVideoByIsLiveAndCategoryId Method

--- backend/src/main/java/com/ssafy/spotlive/api/service/MainServiceImpl.java
+++ backend/src/main/java/com/ssafy/spotlive/api/service/MainServiceImpl.java
@@ -1,11 +1,15 @@
 package com.ssafy.spotlive.api.service;
 
+import com.ssafy.spotlive.api.response.main.VideoFindMainVideoRes;
 import com.ssafy.spotlive.api.response.main.VideoGetRes;
 import com.ssafy.spotlive.db.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * @FileName : MainServiceImpl
@@ -47,5 +51,38 @@ public class MainServiceImpl implements MainService {
             return VideoGetRes.of(videoRepository.findVideosByIsLive(pageRequest, false), pageRequest, sort);
         else
             return VideoGetRes.of(videoRepository.findVideosByIsLiveAndCategory_CategoryId(pageRequest, false, categoryId), pageRequest, sort);
+    }
+
+    @Override
+    public VideoGetRes findAllLiveVideoByIsLiveAndCategoryId(int page, int size, Long categoryId){
+        /**
+         * @Method Name : findAllLiveVideoByIsLiveAndCategoryId
+         * @작성자 : 강용수
+         * @Method 설명 : Query Parameter 조건에 맞는 라이브 영상들을 시청자 수 순으로 검색하는 메소드
+         */
+        PageRequest pageRequest = PageRequest.of(page, size);
+
+        VideoGetRes videoGetRes = null;
+        if (categoryId == null)
+            videoGetRes = VideoGetRes.of(videoRepository.findVideosByIsLive(pageRequest, true), pageRequest, null);
+        else
+            videoGetRes = VideoGetRes.of(videoRepository.findVideosByIsLiveAndCategory_CategoryId(pageRequest, true, categoryId), pageRequest, null);
+
+        Collections.sort(videoGetRes.getVideoResList(), new Comparator<VideoFindMainVideoRes>() {
+            @Override
+            public int compare(VideoFindMainVideoRes v1, VideoFindMainVideoRes v2) {
+                long hit1 = v1.getHitLive();
+                long hit2 = v2.getHitLive();
+
+                if (hit1 < hit2)
+                    return 1;
+                else if (hit1 == hit2)
+                    return 0;
+                else
+                    return -1;
+            }
+        });
+
+        return videoGetRes;
     }
 }
