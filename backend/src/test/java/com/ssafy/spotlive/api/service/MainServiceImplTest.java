b03742be4f7507864dddf21e4d590e695d07903f
ysys <emoney96@naver.com>
2021-08-04 22:06:32 +0900

    🔧 Add MainServiceImplTest findAllFollowVideoByCategoryId Method

--- backend/src/test/java/com/ssafy/spotlive/api/service/MainServiceImplTest.java
+++ backend/src/test/java/com/ssafy/spotlive/api/service/MainServiceImplTest.java
@@ -1,6 +1,9 @@
 package com.ssafy.spotlive.api.service;
 
+import com.ssafy.spotlive.db.entity.Follow;
+import com.ssafy.spotlive.db.entity.User;
 import com.ssafy.spotlive.db.entity.Video;
+import com.ssafy.spotlive.db.repository.UserRepository;
 import com.ssafy.spotlive.db.repository.VideoRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,12 +14,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
-import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MainServiceImplTest {
     @Mock
     private VideoRepository videoRepository;
+
+    @Mock
+    private UserRepository userRepository;
 
     @Mock
     private Page<Video> pageVideo;
@@ -87,5 +97,36 @@ class MainServiceImplTest {
         // then
         verify(videoRepository).findVideosByIsLive(pageRequest, isLive);
         verify(videoRepository).findVideosByIsLiveAndCategory_CategoryId(pageRequest, isLive, categoryId1);
+    }
+
+    @Test
+    void findAllFollowVideoByCategoryId() {
+        // given
+        int page = 0;
+        int size = 3;
+        Long categoryId1 = 6L;
+        Long categoryId2 = null;
+        String accountEmail = "emoney96@naver.com";
+        List<String> accountEmailList = new ArrayList<>();
+
+        User user = new User();
+        user.setAccountEmail("emoney96@naver.com");
+        Sort sort = Sort.by(Sort.Direction.DESC, "videoId");
+        PageRequest pageRequest = PageRequest.of(page, size, sort);
+        Follow follow = new Follow();
+        follow.setFan(user);
+        user.setAccountEmail("ahyen@naver.com");
+        follow.setArtist(user);
+
+        // when
+        when(videoRepository.findVideosByUser_AccountEmailIn(pageRequest, accountEmailList)).thenReturn(pageVideo);
+        when(videoRepository.findVideosByCategory_CategoryIdAndUser_AccountEmailIn(pageRequest, categoryId1, accountEmailList)).thenReturn(pageVideo);
+        when(userRepository.findUserByAccountEmail(accountEmail)).thenReturn(user);
+        mainServiceImpl.findAllFollowVideoByCategoryId(page, size, categoryId1, accountEmail);
+        mainServiceImpl.findAllFollowVideoByCategoryId(page, size, categoryId2, accountEmail);
+
+        // then
+        verify(videoRepository).findVideosByUser_AccountEmailIn(pageRequest, accountEmailList);
+        verify(videoRepository).findVideosByCategory_CategoryIdAndUser_AccountEmailIn(pageRequest, categoryId1, accountEmailList);
     }
 }
