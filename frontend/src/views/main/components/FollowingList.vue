2ed47c6c7ba7919c15309a1d24cb70c5942ce520
MinahLee <ma0723@naver.com>
2021-08-04 16:36:08 +0900

    🔧Add toggle feature at MainSidebar.vue and FollowingList.vue

--- frontend/src/views/main/components/FollowingList.vue
+++ frontend/src/views/main/components/FollowingList.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <router-link class="nav-link" :to="{ name: 'Profile', param: { user_id : following.id } }">
-      <li class="nav-item">
-        <div><img src="~@/assets/icon-streaming.png" class="iconimg"></div>
-      </li>
-    </router-link>
+  <div>    
+    <li class="nav-item">
+      <router-link class="nav-link" :to="{ name: 'Profile', param: { userId : following.id } }">
+        <div>
+          <div><img src="~@/assets/icon-streaming.png" class="sidebar-nav-img"></div>
+          <div v-if="open" class="sidebar-nav-name">{{following.Nickname}}</div>
+        </div>
+      </router-link>
+    </li>
   </div>
 </template>
 
@@ -16,6 +19,10 @@ export default {
       type: Object,
       required: true
     },
+    open: {
+      type: Boolean,
+      required: true
+    },
   },
   data: function () {
   },
@@ -25,5 +32,12 @@ export default {
 </script>
 
 <style>
-
+.sidebar-nav-img {
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+}
+.sidebar-nav-name{
+  font-size: 10px;
+}
 </style>
--- frontend/src/views/main/components/MainSidebar.vue
+++ frontend/src/views/main/components/MainSidebar.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="bgcolor-deep-grey nav-sidebar">
+  <div class="bgcolor-deep-grey nav-sidebar close-sidebar">
     <nav class="nav flex-column">
-      <li class="nav-item side-item">
-        <div v-if="!sidebarOpen"><img src="~@/assets/icon-sidebar.png" class="sidebar-icon-img"></div>
-        <div v-if="sidebarOpen"><img src="~@/assets/icon-sidebar-back.png" class="sidebar-icon-img"></div>
-      </li>
+      <div >
+        <div v-if="!open" @click="openSidebar">
+          <img src="~@/assets/icon-sidebar.png" class="sidebar-icon-img">
+        </div>
+        <div v-if="open" @click="closeSidebar" class="sidebar-close-item">
+          <img src="~@/assets/icon-sidebar-back.png" class="sidebar-icon-img">
+        </div>
+      </div>
       <ul class="navbar-nav" v-for="(following, idx) in followings" :key="idx">
         <FollowingList
           :following="following"
@@ -25,11 +29,27 @@ export default {
   data: function () {
     return {
       followings: [],
-      sidebarOpen: true,
+      open: false,
     }
   },
   methods: {
     getFollowings: function () {
+    },
+    openSidebar: function () {
+      const nav = document.querySelectorAll('.nav-sidebar')[0]
+      if (nav.classList.contains('close-sidebar')) {
+        nav.classList.remove('close-sidebar')
+      } 
+      nav.classList.add('open-sidebar')
+      this.open = true
+    },
+    closeSidebar: function () {
+      const nav = document.querySelectorAll('.nav-sidebar')[0]
+      if (nav.classList.contains('open-sidebar')) {
+        nav.classList.remove('open-sidebar')
+      } 
+      nav.classList.add('close-sidebar')
+      this.open = false
     },
   },
   created: function () {
@@ -41,15 +61,21 @@ export default {
 </script>
 
 <style>
-.nav-sidebar {
+.open-sidebar {
+  width: 300px;
+  display: block;
+  padding: 5px;
+}
+.close-sidebar {
   width: 65px;
   display: block;
   padding: 5px;
 }
-.sidebar-item {
+.sidebar-close-item {
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row-reverse;
+  justify-content: end;
+  margin: 5px;
 }
 .sidebar-icon-img {
   width: 15px;
