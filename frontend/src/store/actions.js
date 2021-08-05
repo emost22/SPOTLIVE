5e326bcafd10f9504b92bc64c525c0e120740a01
kimminkwon <kmk130519@gmail.com>
2021-08-05 23:30:40 +0900

    🔧 Add method for get follow videos in actions

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -51,4 +51,11 @@ export default {
 
         return $axios.get(URL, { params: { page: PAGE_VALUE, size: SIZE_VALUE }})
     },
+    requestGetUserVideos(context, payload) {
+        const URL = '/main/user'
+        const PAGE_VALUE = payload.pageValue;
+        const SIZE_VALUE = payload.sizeValue;
+
+        return $axios.get(URL, { params: { page: PAGE_VALUE, size: SIZE_VALUE }})
+    },
 }
