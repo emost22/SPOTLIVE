05024da88229a26aac200c20e20b46ac45b5377f
MinahLee <ma0723@naver.com>
2021-08-05 17:56:49 +0900

    🔨Modify MainSidebar.vue width of sidebar

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,11 +3,11 @@ module.exports = {
     https: false,
     port: 8083,
     open: true,
-    proxy: {
-      // '/api': {
-      //   target: 'http://localhost:8080/'
-      // },
-    },
+    // proxy: {
+    //   // '/api': {
+    //   //   target: 'http://localhost:8080/'
+    //   // },
+    // },
     historyApiFallback: true,
     hot: true
   },
