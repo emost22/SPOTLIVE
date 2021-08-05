7f51fddb7c368970d6b4033551b7686807d8357f
kimminkwon <kmk130519@gmail.com>
2021-08-05 14:50:46 +0900

    🔧 Add method for join video in user video service

--- backend/src/main/java/com/ssafy/spotlive/api/service/UserVideoServiceImpl.java
+++ backend/src/main/java/com/ssafy/spotlive/api/service/UserVideoServiceImpl.java
@@ -3,21 +3,28 @@ package com.ssafy.spotlive.api.service;
 import com.ssafy.spotlive.db.entity.UserVideo;
 import com.ssafy.spotlive.db.repository.UserVideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @FileName : UserVideoService
  * @작성자 : 김민권
  * @Class 설명 : UserVideo 관련 비즈니스 로직을 처리하는 Service
  */
+@Service
 public class UserVideoServiceImpl implements UserVideoService {
 
     @Autowired
     UserVideoRepository userVideoRepository;
 
     @Override
-    public boolean isInOtherVideo(String accountEmail) {
-
-        return false;
+    public UserVideoRes joinUserVideo(String accountEmail, long videoId) {
+        /**
+         * @Method Name : joinUserVideo
+         * @작성자 : 김민권
+         * @Method 설명 : accountEmail, videoId를 UserVideo Table에 저장한다.
+         */
+        UserVideoReq userVideoReq = makeUserVideoReq(accountEmail, videoId);
+        return UserVideoRes.of(userVideoRepository.save(userVideoReq.toUserVideo()));
     }
 
     @Override
