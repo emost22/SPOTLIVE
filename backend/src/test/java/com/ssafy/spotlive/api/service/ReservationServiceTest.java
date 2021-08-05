7e110a8ddbbcb2a167c4065542da2d6e59953e9a
ahhyeon <ahyendkgus@gmail.com>
2021-08-05 15:25:13 +0900

    🔧 Create ReservationServiceTest and Add insertReservationTest

---
+++ backend/src/test/java/com/ssafy/spotlive/api/service/ReservationServiceTest.java
@@ -0,0 +1,49 @@
+package com.ssafy.spotlive.api.service;
+
+import com.ssafy.spotlive.db.repository.ReservationRepository;
+import com.ssafy.spotlive.db.repository.TimetableRepository;
+import com.ssafy.spotlive.db.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ReservationServiceTest {
+
+    @Mock
+    private ReservationRepository reservationRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private TimetableRepository timetableRepository;
+
+    @InjectMocks
+    private ReservationService reservationService = new ReservationServiceImpl();
+
+    @Test
+    void insertReservationTest() {
+        String accountEmail = "kmk130519@naver.com";
+        Long timetableId = 10L;
+
+//        User user = new User();
+//        user.setAccountEmail(accountEmail);
+//        Timetable timetable = new Timetable();
+//        timetable.setTimetableId(timetableId);
+//        Reservation reservation = new Reservation();
+//        reservation.setUser(user);
+
+//        when(userRepository.findById(accountEmail)).thenReturn(Optional.ofNullable(user));
+//        when(timetableRepository.findById(timetableId)).thenReturn(Optional.ofNullable(timetable));
+        reservationService.insertReservation(accountEmail, timetableId);
+
+        verify(userRepository).findById(accountEmail);
+        verify(timetableRepository).findById(timetableId);
+//        verify(reservationRepository).save(reservation);
+    }
+}
