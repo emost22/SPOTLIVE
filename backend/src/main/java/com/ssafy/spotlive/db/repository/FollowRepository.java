ce70bd561a248480b8f9b5c67c60b3862c9a51fc
sqk8657 <sqk86577@gmail.com>
2021-08-05 16:11:18 +0900

    🔧 Add FollowServiceTest for unfollow, follow

--- backend/src/main/java/com/ssafy/spotlive/db/repository/FollowRepository.java
+++ backend/src/main/java/com/ssafy/spotlive/db/repository/FollowRepository.java
@@ -1,9 +1,11 @@
 package com.ssafy.spotlive.db.repository;
 
 import com.ssafy.spotlive.db.entity.Follow;
+import com.ssafy.spotlive.db.entity.FollowId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
--- backend/src/test/java/com/ssafy/spotlive/api/service/FollowServiceImplTest.java
+++ backend/src/test/java/com/ssafy/spotlive/api/service/FollowServiceImplTest.java
@@ -1,12 +1,14 @@
 package com.ssafy.spotlive.api.service;
 
 import com.ssafy.spotlive.db.entity.Follow;
+import com.ssafy.spotlive.db.entity.FollowId;
 import com.ssafy.spotlive.db.repository.FollowRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,12 +23,38 @@ class FollowServiceImplTest {
     FollowRepository followRepository;
 
     @Test
-    void insertUserByUser() {
-//        followRepository.deleteAll();
-        followService.insertFollowByAccountEmail("sqk8657@naver.com",
-                "kmk130519@naver.com");
-        Optional<Follow> follow = followRepository
-                .findFollowsByArtistAccountEmailAndFanAccountEmail("sqk8657@naver.com", "kmk130519@naver.com");
-        assertThat(follow.get().getFan().getAccountEmail()).isEqualTo("kmk130519@naver.com");
+    void 팔() {
+        // 데이터 셋팅
+        FollowId followId = new FollowId();
+        followId.setArtist("sqk8657@naver.com");
+        followId.setFan("kmk130519@naver.com");
+
+        // 팔로우 실행
+        followService.insertFollowByAccountEmail("sqk8657@naver.com","kmk130519@naver.com");
+
+        // 영린 민권 쌍이 있는지 확인
+        Optional<Follow> follow = followRepository.findById(followId);
+        assertThat(follow.isPresent()).isEqualTo(true);
+    }
+
+    @Test
+    void 언팔() {
+        // 데이터 셋팅
+        FollowId followId = new FollowId();
+        followId.setArtist("sqk8657@naver.com");
+        followId.setFan("kmk130519@naver.com");
+
+        // 영린 민권 쌍이 있나 테스트
+        Optional<Follow> follow = followRepository.findById(followId);
+        assertThat(follow.isPresent()).isEqualTo(true);
+
+        // 언팔로우 실행
+        followService.deleteFollowByAccountEmail("sqk8657@naver.com","kmk130519@naver.com");
+
+        // 이제는 없으면 성공
+        Optional<Follow> follow2 = followRepository.findById(followId);
+        assertThat(follow2.isPresent()).isEqualTo(false);
+    }
+
     }
 }
