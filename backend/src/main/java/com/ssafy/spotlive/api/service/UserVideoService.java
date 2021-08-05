43eaca609b41d11d6cc8d338be36e25fb9fea561
kimminkwon <kmk130519@gmail.com>
2021-08-05 14:49:48 +0900

    🏗️ Modify structure of user video service

--- backend/src/main/java/com/ssafy/spotlive/api/service/UserVideoService.java
+++ backend/src/main/java/com/ssafy/spotlive/api/service/UserVideoService.java
@@ -1,6 +1,6 @@
 package com.ssafy.spotlive.api.service;
 
-import com.ssafy.spotlive.db.entity.UserVideo;
+import com.ssafy.spotlive.api.response.uservideo.UserVideoRes;
 
 /**
  * @FileName : UserVideoService
@@ -8,7 +8,6 @@ import com.ssafy.spotlive.db.entity.UserVideo;
  * @Class 설명 : UserVideo 관련 비즈니스 로직을 처리하는 Service의 Interface
  */
 public interface UserVideoService {
-    boolean isInOtherVideo(String accountEmail);
-    UserVideo joinUserVideo(String accountEmail, Long videoId);
-    UserVideo leaveUserVideo(String accountEmail, Long videoId);
+    UserVideoRes joinUserVideo(String accountEmail, long videoId);
+    void leaveUserVideo(String accountEmail, long videoId);
 }
