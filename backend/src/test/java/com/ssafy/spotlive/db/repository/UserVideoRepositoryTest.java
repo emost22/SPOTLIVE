0942afb4c6737fe0bebedb4519581068d47b837c
kimminkwon <kmk130519@gmail.com>
2021-08-05 11:36:22 +0900

    🔧 Add test for findByPK in user video repository

--- backend/src/test/java/com/ssafy/spotlive/db/repository/UserVideoRepositoryTest.java
+++ backend/src/test/java/com/ssafy/spotlive/db/repository/UserVideoRepositoryTest.java
@@ -2,11 +2,13 @@ package com.ssafy.spotlive.db.repository;
 
 
 import com.ssafy.spotlive.db.entity.UserVideo;
+import com.ssafy.spotlive.db.entity.UserVideoId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -28,6 +30,19 @@ class UserVideoRepositoryTest {
 
     @Test
     void findByIdTest() {
+        // given
+        String accountEmail = "emoney96@naver.com";
+        long videoId = 8;
+        UserVideoId userVideoId = new UserVideoId();
+        userVideoId.setVideo(videoId);
+        userVideoId.setUser(accountEmail);
 
+        // when
+        Optional<UserVideo> optionalUserVideo = userVideoRepository.findById(userVideoId);
+
+        // then
+        assertThat(optionalUserVideo).isNotEqualTo(Optional.empty());
+        assertThat(optionalUserVideo.get().getUser().getAccountEmail()).isEqualTo(accountEmail);
+        assertThat(optionalUserVideo.get().getVideo().getVideoId()).isEqualTo(videoId);
     }
 }
