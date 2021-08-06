7f75b2325490a12f8c96ef5376b4878b327b67de
kimminkwon <kmk130519@gmail.com>
2021-08-06 17:24:46 +0900

    🏗️ Modify structure for convention

--- frontend/src/views/profile/Profile.vue
+++ frontend/src/views/profile/Profile.vue
@@ -81,7 +81,7 @@ export default {
     this.getMyProfile()
     this.getUser()
   },
-  computed: function () {
+  computed: {
   },
 }
 </script>
--- frontend/src/views/search/Search.vue
+++ frontend/src/views/search/Search.vue
@@ -26,7 +26,7 @@ export default {
   created: function () {
     this.getSearchs()
   },
-  computed: function() {
+  computed: {
 
   },
 }
