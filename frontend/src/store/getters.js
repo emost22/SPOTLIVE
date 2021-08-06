393005ed279322bbf2f5411d689495ace52fc786
kimminkwon <kmk130519@gmail.com>
2021-08-06 16:18:35 +0900

    🔧 Add getter for openvidu variable in state

--- frontend/src/store/getters.js
+++ frontend/src/store/getters.js
@@ -5,6 +5,32 @@ export default {
     },
     isLogin(state) {
         return state.isLogin
-    }
-
+    },
+    ovSessionId(state) {
+        return state.ovSessionId
+    },
+    ovToken(state) {
+        return state.ovToken
+    },
+    OV(state) {
+        return state.OV
+    },
+    ovSession(state) {
+        return state.ovSession
+    },
+    videoDevices(state) {
+        return state.videoDevices
+    },
+    audioDevices(state) {
+        return state.audioDevices
+    },
+    mainStreamManager(state) {
+        return state.mainStreamManager
+    },
+    publisher(state) {
+        return state.publisher
+    },
+    subscribers(state) {
+        return state.subscribers
+    },
 }
