114b75a38c808e9437795f0e60b5b11983070a11
MinahLee <ma0723@naver.com>
2021-08-06 18:24:11 +0900

    🔨Modify thumbnail img tag at VideoCard.vue and VideoCarouselCard.vue

---
+++ frontend/src/views/profile/components/MyShow.vue
@@ -0,0 +1,39 @@
+<template>
+  <carousel-3d>
+    <slide v-for="(show, i) in shows" :index="i" :key="i">
+      <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
+          <MyShowCard
+            :data-index="index" 
+            :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }" 
+            :show="show"
+          />
+      </template>
+    </slide>
+  </carousel-3d>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d'
+import MyShowCard from '@/views/profile/components/MyShowCard.vue'
+
+export default {
+  name: "MyShow",
+  props: {
+    shows: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    Carousel3d,
+    Slide,
+    MyShowCard,
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/profile/components/MyShowCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img :src="show.thumbnailUrl" @click="ShowDetail">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyShowCard",
+  props: {
+    show: {
+      type: Object,
+      required: true
+    },
+  },
+  methods: {
+    ShowDetail: function () {
+    },
+  },
+}
+</script>
+
+<style>
+</style>
