c53fd4fc9a63ef8445ed263e5c3ccf7a419b9ca5
kimminkwon <kmk130519@gmail.com>
2021-08-08 21:01:41 +0900

    🔧 Add function for set loginUser in localstroage

--- frontend/src/store/mutations.js
+++ frontend/src/store/mutations.js
@@ -8,9 +8,12 @@ export default {
     DO_KAKAO_LOGIN(state, payload) {
         console.log("MUTATION: DO_KAKAO_LOGIN() RUN...")
         console.log(payload)
+
         state.loginUser = payload.loginUser
         state.isLogin = true
+        localStorage.setItem('loginUser', JSON.stringify(state.loginUser))
         localStorage.setItem('accessToken', payload.loginUser.accessToken)
+        
         $axios.defaults.headers['Authorization'] = 'Bearer ' + payload.loginUser.accessToken
         router.push({ name: "Main" })
     },
