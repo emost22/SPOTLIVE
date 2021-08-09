87c825abc19b4f5258d89437945ca2d21e836b31
ahhyeon <ahyendkgus@gmail.com>
2021-08-09 13:50:17 +0900

    🔧 Add findShowInfoByUser method in ShowInfoService

--- backend/src/main/java/com/ssafy/spotlive/api/service/ShowInfoService.java
+++ backend/src/main/java/com/ssafy/spotlive/api/service/ShowInfoService.java
@@ -3,7 +3,10 @@ package com.ssafy.spotlive.api.service;
 import com.ssafy.spotlive.api.request.showInfo.ShowInfoInsertPostReq;
 import com.ssafy.spotlive.api.request.showInfo.ShowInfoUpdatePatchReq;
 import com.ssafy.spotlive.api.response.showInfo.ShowInfoFindByIdGetRes;
+import com.ssafy.spotlive.api.response.showInfo.ShowInfoRes;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * @FileName : ShowInfoService
@@ -15,4 +18,5 @@ public interface ShowInfoService {
     ShowInfoFindByIdGetRes findShowInfoById(long id);
     Long deleteShowInfoById(long id);
     Boolean updateShowInfoById(long id, ShowInfoUpdatePatchReq showInfoUpdatePatchReq, MultipartFile posterImage);
+    List<ShowInfoRes> findShowInfoByUser(String accountEmail);
 }
