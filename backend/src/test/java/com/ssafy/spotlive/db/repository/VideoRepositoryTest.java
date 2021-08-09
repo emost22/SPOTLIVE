dfda0ac5a6da36f1fdb7da2bb74f0caa35c6ec78
ysys <emoney96@naver.com>
2021-08-09 17:54:47 +0900

    🔨 Fix VideoRepositoryTest findVideosByCategory_CategoryIdAndUser_AccountEmailIn Method

--- backend/src/test/java/com/ssafy/spotlive/db/repository/VideoRepositoryTest.java
+++ backend/src/test/java/com/ssafy/spotlive/db/repository/VideoRepositoryTest.java
@@ -231,20 +231,40 @@ public class VideoRepositoryTest {
     @Test
     void findVideosByCategory_CategoryIdAndUser_AccountEmailIn(){
         // given
+        String insertVideoTitle = "알고리즘 잘하는법";
+        String insertVideoDescription = "kmk님이 알려주실겁니다";
+        String insertMode = "소통";
+        Long insertCategoryId = 2L;
+        Long insertShowInfoId = 6L;
+        String insertAccountEmail = "emoney96@naver.com";
+        String insertSessionId = "123123";
+        String insertThumbnailUrl = "solvedac.png";
+
+        VideoInsertPostReq videoInsertPostReq = new VideoInsertPostReq();
+        videoInsertPostReq.setVideoTitle(insertVideoTitle);
+        videoInsertPostReq.setVideoDescription(insertVideoDescription);
+        videoInsertPostReq.setMode(insertMode);
+        videoInsertPostReq.setCategoryId(insertCategoryId);
+        videoInsertPostReq.setShowInfoId(insertShowInfoId);
+        videoInsertPostReq.setAccountEmail(insertAccountEmail);
+        videoInsertPostReq.setSessionId(insertSessionId);
+
         int page = 0;
-        int size = 3;
+        int size = MAX;
         List<String> accountEmailList = new ArrayList<>();
         accountEmailList.add("sqk8657@naver.com");
         accountEmailList.add("ahyen@naver.com");
-        Long categoryId = 6L;
+        accountEmailList.add("emoney96@naver.com");
+        Long categoryId = 2L;
         Sort sort = Sort.by(Sort.Direction.DESC, "videoId");
         PageRequest pageRequest = PageRequest.of(page, size, sort);
 
         // when
+        Video video = videoRepository.save(videoInsertPostReq.toVideo(insertThumbnailUrl));
         Page<Video> pageVideo = videoRepository.findVideosByCategory_CategoryIdAndUser_AccountEmailIn(pageRequest, categoryId, accountEmailList);
 
         // then
-        assertThat(pageVideo.getTotalElements()).isEqualTo(4);
+        assertThat(pageVideo.getContent().stream().anyMatch(newVideo -> newVideo.getVideoId() == video.getVideoId())).isEqualTo(true);
     }
 
     @Test
