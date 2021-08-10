0563252fdb46f087765ded8ff60a9428cb697b93
kimminkwon <kmk130519@gmail.com>
2021-08-11 02:02:33 +0900

    🔧 Add function for start recording in actions

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -116,6 +116,17 @@ export default {
 
         return $axios.post(URL, JSON.stringify(payload), { auth: AUTH_REQ })
     },
+
+    requestEndRecording(context, payload) {
+        const OPENVIDU_URL = "https://i5a405.p.ssafy.io"
+        const AUTH_REQ = {
+            username: 'OPENVIDUAPP',
+            password: 'MY_SECRET',
+        }
+        const URL = OPENVIDU_URL + '/openvidu/api/recordings/stop/' + payload.ovSessionId
+
+        return $axios.post(URL, JSON.stringify({}), { auth: AUTH_REQ })
+    },
     // RoomSettingDialogForm.vue
     requestGetRecentlyTimeTable(context, payload) {
         const URL = `/showinfo/timetable/${payload.showInfoId}`
