5db59a5de5a23ff84ee5769eaf7ab0cb5b5c8e57
ysys <emoney96@naver.com>
2021-08-12 14:10:16 +0900

    🔧 Create ShowUpdateDialog.vue

---
+++ frontend/src/views/profile/components/ShowUpdateDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="modal fade" id="showUpdateModal" ref="showUpdateModal" tabindex="-1" aria-labelledby="showUpdateModalLabel" aria-hidden="true">
+      <div class="modal-dialog bdcolor-bold-npurple modal-design">
+        showdata수정
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ShowUpdateDialog',
+  data: function() {
+    return {
+    }
+  },
+  created: function () {
+  },
+  mounted() {
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  },
+  watch: {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
