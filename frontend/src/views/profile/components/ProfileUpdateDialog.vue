f00f3badab1c584c2788390546ba977704fa7b18
ysys <emoney96@naver.com>
2021-08-13 22:30:55 +0900

    🔨 Add ProfileUpdateDialog.vue getMyProfile Method and use createdProfileData

--- frontend/src/views/profile/components/ProfileUpdateDialog.vue
+++ frontend/src/views/profile/components/ProfileUpdateDialog.vue
@@ -74,14 +74,34 @@ export default {
         .then((response) => {
           console.log(response.data);
           localStorage.setItem('loginUser', JSON.stringify(response.data));
+          this.getMyProfile()
         })
         .catch((error) => {
           console.log(error);
         });
     },
+    getMyProfile() {
+      this.$store.dispatch('requestGetMyProfile')
+      .then((response) => {
+        console.log("getMyProfile() SUCCESS!!")
+        console.log(response.data)
+        var ProfileData = {
+          myProfile : response.data,
+          following : response.data.followMyArtistResList,
+          follower : response.data.followMyFanResList,
+          myShows : response.data.showInfoResList,
+          myVideos : response.data.videoResList,
+          myReservations : response.data.reservationResList,
+        }
+        this.$store.dispatch('requestSetCreatedProfileData', ProfileData)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    },
   },
   computed: {
-    ...mapGetters(['loginUser']),
+    ...mapGetters(['loginUser', 'createdProfileData']),
   },
 };
 </script>
