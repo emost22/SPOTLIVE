b6e4a121061838f4a5ce1220cd74005d69c5e763
MinahLee <ma0723@naver.com>
2021-08-14 21:55:50 +0900

    🎨Modify filter Button css when clicking filters

--- frontend/src/views/main/components/FilterButton.vue
+++ frontend/src/views/main/components/FilterButton.vue
@@ -22,7 +22,17 @@ export default {
   methods: {
     clickFilter: function () {
       this.$emit("categoryId", this.filter.categoryId)
-      // 해당 버튼 색 변화
+      const filters = document.querySelectorAll('.filter-btn')
+      console.log(filters)
+      console.log(filters[this.idx])
+      filters.forEach((filter) => { 
+        if (filter.classList.contains('bgcolor-filter-click')) {
+          filter.classList.remove('bgcolor-filter-click')
+          filter.classList.add('bgcolor-deep-grey')
+        } 
+      })
+      filters[this.idx].classList.remove('bgcolor-deep-grey')
+      filters[this.idx].classList.add('bgcolor-filter-click')
     },
   },
   computed: {
@@ -40,6 +50,13 @@ export default {
   font-size: 18px;
 }
 .filter-btn:hover {
+  border: 2px solid #04F7CA;
+  box-shadow: 
+    0 0 9px #04F7CA,
+    0 0 12px #04F7CA;
+}
+.bgcolor-filter-click {
+  background-color: #242424;
   border: 2px solid #04F7CA;
   box-shadow: 
     0 0 9px #04F7CA,
